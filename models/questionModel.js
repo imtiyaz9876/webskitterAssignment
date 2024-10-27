1e5fd023b34dd58232c3c348075710100c10d278
imtiyaz9876 <md.imtiyazalam9876@gmail.com>
2024-10-27 10:50:02 +0530

    indexing added on question schema on catagories and question search

--- models/questionModel.js
+++ models/questionModel.js
@@ -1,11 +1,15 @@
+// questionModel.js
 const mongoose = require('mongoose');
 
-
-// const user = mongoose.Schema(
-const questionSchema = mongoose.Schema({
+const questionSchema = new mongoose.Schema({
     question: { type: String, required: true }, // The question text
     categories: [{ type: String, required: true }], // Use an array for multiple categories
 });
 
+// Create an index on the `categories` array field to speed up queries
+questionSchema.index({ categories: 1 });
+
+// Optional: Create a full-text index on the `question` field for text search
+questionSchema.index({ question: 'text' });
 
 module.exports = mongoose.model('Questions', questionSchema);
